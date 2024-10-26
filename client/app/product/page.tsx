f481f336ae757fdacea8737f65b13da41daea26c
ishola360 <isholah360.com>
2024-10-26 10:16:38 +0100

    procardline

--- client/app/product/page.tsx
+++ client/app/product/page.tsx
@@ -1,25 +1,14 @@
 
 
 import React from "react";
-import AllProduct from "@/lib/api";
 import ProductList from "../../compoent/ProductList";
 
-interface Product {
-  id: string;
-  thumbnail: string;
-  title: string;
-  price: number;
-  description: string;
- 
-}
-
 export default async function Products() {
-  const productData = await AllProduct(); 
-  const products: Product[] = productData.products || []; 
+
 
   return (
     <div className="container mx-auto px-4">
-      <ProductList products={products} /> 
+      <ProductList /> 
     </div>
   );
 }
